05463a3f0c9658388775d259fba22a40f7fa54c6
funsizemaddy <90282709+funsizemaddy@users.noreply.github.com>
2022-10-03 11:59:28 -0600

    new code for correct word guessed

--- Wordle.py
+++ Wordle.py
@@ -6,7 +6,9 @@
 # Create a project like wordle in python
 #
 
+from asyncio import events
 import random
+from this import s
 
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS,CORRECT_COLOR,PRESENT_COLOR,MISSING_COLOR
@@ -20,10 +22,12 @@ def wordle():
             gw.show_message("Congrats, that is in the word list!")
         else:
             gw.show_message("Not in word list")
+
 #parse submitted word into letters for easier functionality
         sLetters = [*s]
         for y, x in enumerate(sLetters):
 # check if the letter is in the right position of the word
+
             if x == keyLetters[y]:
                 color = CORRECT_COLOR
 # check to see if the letter is in any position for the word
@@ -34,6 +38,13 @@ def wordle():
                 color = MISSING_COLOR
 # Change the color of the squre to the assigned letter
             gw.set_square_color(0,y,color) ##############This will have to change once we start working with different rows -----------------------------------------------------------------------------------------------
+# check if user entered correct word
+        if  keyword == s:
+# Congratulatory message
+            gw.show_message("Congrats, you guessed the WORDLE")
+        else:
+# move onto next line if word not guess correctly ( set_current_row and get_current_row) 
+            gw.get_current_row()
 # initiate wordle window and make enter button active
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
@@ -44,13 +55,8 @@ def wordle():
 #loop through letters to input on first row of wordle screen
     for y, x in enumerate(keyLetters):
         gw.set_square_letter(0,y,x)
-# check if user entered correct word
-    if keyLetters == keyword:
-# Congratulatory message
-        gw.show_message("Congrats, you guessed the WORDLE")
-    else: 
-# move onto next line if word not guess correctly ( set_current_row and get_current_row) 
-        gw.get_current_row()
+
+
 
 
 
